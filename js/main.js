05173d03d9b1c8cf667f13249c55f66fd57818ed
Caleb Eby <caleb.eby01@gmail.com>
2016-01-27 19:08:15 -0800

    fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Buttons
 var parent, ink, d, x, y;
-$("ul li a, .button a").mousedown(function(e){
+$("ul li a, .button").mousedown(function(e){
 	parent = $(this).parent();
 	//create .ink element if it doesn't exist
 	if(parent.find(".ink").length === 0)
