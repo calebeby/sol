8e8c4b6f150627ccc743cf987f1c1853cc2c845c
Caleb Eby <caleb.eby01@gmail.com>
2016-01-28 20:10:04 -0800

    change

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-(function (window, $) {
+(function(window, $) {
 
   $(function() {
 
-
-    $('.ripple').on('click', function (event) {
+    $('.ripple').on('click', function(event) {
       event.preventDefault();
 
       var $div = $('<div/>'),
-          btnOffset = $(this).offset(),
-      		xPos = event.pageX - btnOffset.left,
-      		yPos = event.pageY - btnOffset.top;
-
-
+        btnOffset = $(this).offset(),
+        xPos = event.pageX - btnOffset.left,
+        yPos = event.pageY - btnOffset.top;
 
       $div.addClass('ripple-effect');
       var $ripple = $(".ripple-effect");
@@ -20,13 +17,13 @@
       $ripple.css("width", $(this).height());
       $div
         .css({
-          top: yPos - ($ripple.height()/2),
-          left: xPos - ($ripple.width()/2),
+          top: yPos - ($ripple.height() / 2),
+          left: xPos - ($ripple.width() / 2),
           background: $(this).data("ripple-color")
         })
         .appendTo($(this));
 
-      window.setTimeout(function(){
+      window.setTimeout(function() {
         $div.remove();
       }, 2000);
     });
