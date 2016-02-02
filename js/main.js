57873ffe33c8ed5d19b3345d07e87c8152ba0ee8
Caleb Eby <caleb.eby01@gmail.com>
2016-02-01 16:08:05 -0800

    update feb 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-Waves.attach('.button', ['waves-block']);
+Waves.attach('.button', 'waves-light');
+Waves.attach('button', 'waves-light');
+Waves.attach('input[type="button"]', 'waves-light');
+Waves.attach('input[type="submit"]', 'waves-light');
+Waves.attach('input[type="reset"]', 'waves-light');
 Waves.init();
--- js/waves.js
+++ js/waves.js
@@ -214,15 +214,6 @@
             // Put element as child
             parent.replaceChild(wrapper, element);
             wrapper.appendChild(element);
-
-            // Apply element color and background color to wrapper
-            var elementStyle    = window.getComputedStyle(element, null);
-            var color           = elementStyle.color;
-            var backgroundColor = elementStyle.backgroundColor;
-
-            wrapper.setAttribute('style', 'color:' + color + ';background:' + backgroundColor);
-            element.setAttribute('style', 'background-color:rgba(0,0,0,0);');
-
         },
 
         // Wrap <img> tag so it can perform the effect
