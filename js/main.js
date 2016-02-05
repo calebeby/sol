a06f70ae7109c2932dd49bde36bb1084995459b1
Caleb Eby <caleb.eby01@gmail.com>
2016-02-04 21:02:30 -0800

    add change update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ Waves.attach('input[type="button"]', 'waves-light');
 Waves.attach('input[type="submit"]', 'waves-light');
 Waves.attach('input[type="reset"]', 'waves-light');
 Waves.init();
+$(function(){
+  if($('input[type="text"]').val() === "") {
+    $(this).addClass(".empty");
+  } else {
+    $(this).removeClass(".empty");
+  }
+});
