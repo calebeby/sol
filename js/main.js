aa942c94253d4196c4e91ebc9958d3aa4c8bd45e
Caleb Eby <caleb.eby01@gmail.com>
2016-02-06 11:52:45 -0800

    update feb 6

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 Waves.attach('.button', 'waves-light');
 Waves.attach('button', 'waves-light');
-Waves.attach('input[type="button"]', 'waves-light');
-Waves.attach('input[type="submit"]', 'waves-light');
-Waves.attach('input[type="reset"]', 'waves-light');
 Waves.init();
+var $textInput = 'input[type="color"], input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="email"], input[type="month"], input[type="number"], input[type="password"], input[type="search"], input[type="tel"], input[type="text"], input[type="time"], input[type="url"], input[type="week"],textarea'
 $(function(){
-  if($('input[type="text"]').val() === "") {
-    $(this).addClass(".empty");
-  } else {
-    $(this).removeClass(".empty");
-  }
+  $($textInput).addClass('empty');
+  $($textInput).focus(function() {
+    $(this).addClass('visited');
+  });
+  $($textInput).on('change', function() {
+    if ($(this).val() == '') {
+      $($(this)).addClass('empty');
+    } else {
+      $($(this)).removeClass('empty');
+    }
+  });
 });
